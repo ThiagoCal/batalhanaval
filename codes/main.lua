92f73bbe9828cfea7e23d75d5a662bd2cb24bb56
Thiago Calazans <thiagotorrescalazans@gmail.com>
2021-04-19 16:51:59 -0300

    atualização dos arquivos

--- codes/main.lua
+++ codes/main.lua
@@ -1,6 +1,39 @@
-local x = 10
-local y = 10
+function love.load()
+        love.window.setMode(800, 600)
+        -- love.graphics.setBackgroundColor(0.36, 0.57, 0.89)
+        -- love.graphics.setColor(0, 0, 0)
+    end
 
-function love.draw()
-    love.graphics.circle( "fill", x, y, 100)
-end
+    function love.update()
+
+    end
+
+    function love.draw()
+        local size = 38
+        local p0 = 10
+        local pf = 390
+
+        for x = p0, pf - 1, size do
+            for y = p0, pf - 1, size do
+                love.graphics.line(x, y, x, y + size)
+                love.graphics.line(x, y, x + size, y)
+            end
+        end
+        love.graphics.line(pf, p0, pf, pf)
+        love.graphics.line(p0, pf, pf, pf)
+    end
+
+	function love.draw()
+        local size = 38
+        local p0 = 400
+        local pf = 390
+
+        for x = p0, pf - 1, size do
+            for y = p0, pf - 1, size do
+                love.graphics.line(x, y, x, y + size)
+                love.graphics.line(x, y, x + size, y)
+            end
+        end
+        love.graphics.line(pf, p0, pf, pf)
+        love.graphics.line(p0, pf, pf, pf)
+    end
