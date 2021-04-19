63f0a2b98f67e296277b129c646a118129c0dece
Thiago Calazans <thiagotorrescalazans@gmail.com>
2021-04-19 17:22:28 -0300

    tabela atualizada

--- codes/main.lua
+++ codes/main.lua
@@ -1,5 +1,5 @@
 function love.load()
-        love.window.setMode(800, 600)
+        love.window.setMode(600,800)
         -- love.graphics.setBackgroundColor(0.36, 0.57, 0.89)
         -- love.graphics.setColor(0, 0, 0)
     end
@@ -8,21 +8,23 @@ function love.load()
 
     end
 
-	function drawboard(pf, pf, size, n)
-	end
-
-    function love.draw()
-        local size = 38
-        local p0 = 10
-        local pf = 390
-
-        for x = p0, pf - 1, size do
-            for y = p0, pf - 1, size do
-                love.graphics.line(x, y, x, y + size)
-                love.graphics.line(x, y, x + size, y)
-            end
+function drawboard(x0, y0, xf, yf, size, n)
+        for x = x0, xf -1, size do
+                for y = y0, yf -1, size do
+                        love.graphics.line(x, y, x, y + size)
+                        love.graphics.line(x, y, x + size, y)
+                end
+        love.graphics.line(x0, yf, xf, yf)
+        love.graphics.line(xf, y0, xf, yf)
         end
-        love.graphics.line(pf, p0, pf, pf)
-        love.graphics.line(p0, pf, pf, pf)
-    end
+end
+
+function love.draw()
+        local size = 38
+        local n = 10
+        drawboard(30, 30, 420, 420,size, n)
+
+
+end
+
 
